57caeaa38eee16dea8cb3c9909476910d6cbbd36
Fdyra04 <indyra.apaza@gmail.com>
2025-01-29 11:30:17 -0500

    Add files via upload

---
+++ ejer3.py
@@ -0,0 +1,5 @@
+edad=input("ingrese su edad: ")
+if(int(edad)>=18):
+    print("Mayor de edad")
+else:
+    print("Menor de edad")
---
+++ ejer5.py
@@ -0,0 +1,8 @@
+# Pedir un número entero al usuario
+number = int(input("Ingresa un número entero: "))
+
+# Calcular la suma de 1 hasta n usando la fórmula de la suma de números enteros
+suma = (number * (number + 1)) / 2
+
+# Resultado concluso
+print(f"La suma de los números de 1 hasta {number} es: {suma}")
